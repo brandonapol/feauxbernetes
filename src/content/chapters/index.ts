22572498a5407139401f170b194315ae61269874
Brandon Apol <apolbrandon@gmail.com>
2026-09-20 08:26:39 -0400

    Ch 3: It turns itself back on
    Unplug a search copy and watch a replacement appear, turn off Box B and
    watch copies move, then scale search to 5. The wrong wish is vetoed by
    Kai. On-call brain: this is why one crashed copy at 3am doesn't wake
    anybody up.
    
    Closes #21

--- src/content/chapters/index.ts
+++ src/content/chapters/index.ts
@@ -2,5 +2,6 @@ import type { Chapter } from '../../engine/story/types'
 import { welcomeChapter } from './00-welcome'
 import { boxesChapter } from './01-boxes'
 import { orderFormChapter } from './02-order-form'
+import { selfHealChapter } from './03-self-heal'
 
-export const CHAPTERS: Chapter[] = [welcomeChapter, boxesChapter, orderFormChapter]
+export const CHAPTERS: Chapter[] = [welcomeChapter, boxesChapter, orderFormChapter, selfHealChapter]
